6ad2e80fc94325623880b878f6552da69e5915a2
Just1689 <Just1689@gmail.com>
2020-01-03 12:08:39 +0200

    Added title to message

--- ws/model.go
+++ ws/model.go
@@ -4,5 +4,6 @@ import "encoding/json"
 
 type WrappedMessage struct {
 	ClientID string          `json:"clientID"`
+	Title    string          `json:"title"`
 	Body     json.RawMessage `json:"body"`
 }
