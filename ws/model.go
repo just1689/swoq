b51b39a91af44302a556a0c7cd94f382c94f964e
Just1689 <Just1689@gmail.com>
2020-01-03 16:59:24 +0200

    Wrong level

--- ws/model.go
+++ ws/model.go
@@ -4,6 +4,5 @@ import "encoding/json"
 
 type WrappedMessage struct {
 	ClientID string          `json:"clientID"`
-	Title    string          `json:"title"`
 	Body     json.RawMessage `json:"body"`
 }
